4be59edfe84ed1242348a6dfe80c833f5ad03758
khanhphanttgitlab <khanh.phan@tokyotechies.com>
2023-02-28 20:48:26 +0900

    test streamlit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+# Takes in a username and prints out a greeting text
+st.title('A Simple Streamlit Web App')
+name = st.text_input('Your name', '')
+if len(name) > 0:
+    st.write(f'Hello {name}!')
+
+st.header("Let's do some math")
+# Takes in two slider inputs of integers ranging from 0 to 10
+x = st.slider('Select an integer x', 0, 10, 3)
+y = st.slider('Select an integer y', 0, 10, 5)
+df = pd.DataFrame(
+    {'x': [x], 'y': [y], 'x + y': [x + y]}, 
+    index = ['addition row'],
+)
+# Prints out the variables and their sums
+st.write(df)

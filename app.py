965d1a88d2683262672c6c8ec4c7554b794b4bb5
khanhphanttgitlab <khanh.phan@tokyotechies.com>
2023-03-01 16:28:00 +0900

    test

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ if len(name) > 0:
 
 st.header("Let's do some math")
 # Takes in two slider inputs of integers ranging from 0 to 10
-x = st.slider('Select an integer x', 0, 10, 3)
-y = st.slider('Select an integer y', 0, 10, 5)
+x = st.slider('Select an integer X', 0, 10, 3)
+y = st.slider('Select an integer Y', 0, 10, 5)
 df = pd.DataFrame(
     {'x': [x], 'y': [y], 'x + y': [x + y]}, 
     index = ['addition row'],
